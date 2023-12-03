41c401551924f7883a571c254ff3bc60798198c5
M <19733552+jenk1@users.noreply.github.com>
2023-12-03 12:29:09 -0500

    make sure db is migrated whenever updated

--- src/lib/supabase/db.ts
+++ src/lib/supabase/db.ts
@@ -2,6 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as dotenv from 'dotenv';
 import * as schema from '../../../migrations/schema';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
 dotenv.config({ path: '.env'});
 
 if(!process.env.DATABASE_URL) {
@@ -10,4 +11,13 @@ if(!process.env.DATABASE_URL) {
 
 const client = postgres(process.env.DATABASE_URL as string, {max: 1});
 const db = drizzle(client, { schema });
+const migrateDb = async () => {
+    try {
+        console.log('Migrate client');
+        await migrate(db, { migrationsFolder: "migrations"});
+        console.log('Successfully Migrated')
+    } catch (error) {
+        console.log('Error Migrating Client')
+    }
+}
 export default db;
