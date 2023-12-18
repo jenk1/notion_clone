715ee04872f300bdad4937eb8f19d15fc3b60bc2
jenk1 <19733552+jenk1@users.noreply.github.com>
2023-12-18 12:49:55 -0500

    get loading component setup

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,10 +5,16 @@ import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { FormSchema } from '@/lib/types';
-import { Form } from '@/components/ui/form';
+import { Form, FormControl, FormDescription, FormField,
+FormItem, FormMessage } from '@/components/ui/form';
 import Image from 'next/image';
 import Link from 'next/link';
 import Logo from '../../../../public/cypresslogo.svg';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import Loader from '@/components/global/Loader';
+//import { Separator } from '@/components/ui/separator';
+//import { actionLoginUser } from '@/lib/server-actions/auth-actions';
 
 const LoginPage = () => {
     const router = useRouter();
@@ -46,6 +52,64 @@ const LoginPage = () => {
                 <Image src={Logo} alt='Logo' width={50} height={50}/>
                 <span className='font-semibold dark:text-white text-4xl ml-2'>Notion Clone</span>
             </Link>
+            <FormDescription
+            className="
+        text-foreground/60"
+        >
+            An all-In-One Collaboration and Productivity Platform
+        </FormDescription>
+        <FormField
+            disabled={isLoading}
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+            <FormItem>
+                <FormControl>
+                <Input
+                    type="email"
+                    placeholder="Email"
+                    {...field}
+                />
+                </FormControl>
+                <FormMessage />
+            </FormItem>
+            )}
+        />
+        <FormField
+            disabled={isLoading}
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+            <FormItem>
+                <FormControl>
+                <Input
+                    type="password"
+                    placeholder="Password"
+                    {...field}
+                />
+                </FormControl>
+                <FormMessage />
+            </FormItem>
+            )}
+        />
+        {submitError && <FormMessage>{submitError}</FormMessage>}
+        <Button
+            type="submit"
+            className="w-full p-6"
+            size="lg"
+            disabled={isLoading}
+        >
+            {!isLoading ? 'Login' : <Loader />}
+        </Button>
+        <span className="self-container">
+            Dont have an account?{' '}
+        <Link
+            href="/signup"
+            className="text-primary"
+        >
+            Sign Up
+        </Link>
+        </span>
         </form>
     </Form>
     )
